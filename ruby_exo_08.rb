0914b9efb697cb06d21f7fcd5e336f846bd7dd1e
CafrineSab <sabmhadji@gmail.com>
2020-09-29 23:31:59 +0200

    add exo 8 et 9

---
+++ ruby_exo_08.rb
@@ -0,0 +1,4 @@
+user_name = "Prénom"
+puts "Bonjour ! Comment tu t'appelles ?"
+print "Bonjour "
+puts  user_name 
---
+++ ruby_exo_09.rb
@@ -0,0 +1,7 @@
+user_name = "Prénom"
+user_lastname = "Nom"
+puts "Quel est ton prénom ?"
+puts "Et ton nom ?"
+print "Bonjour, "
+print user_name +  user_lastname 
+puts "!"
