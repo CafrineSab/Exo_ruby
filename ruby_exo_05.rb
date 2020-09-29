424c3f1afd0e386decdeee92f818f9a71398fc82
CafrineSab <sabmhadji@gmail.com>
2020-09-29 23:01:49 +0200

    add exo 5, 6 et 7

---
+++ ruby_exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" # il s'agit d'une boucle
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # il s'agit d'une boucle
+
+puts "Et en secondes ?" # affiche un texte simple
+
+puts 10 * 5 * 11 * 60 * 60 # affiche un calcul simple
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # c'est une donnée "booléen"
+
+puts 3 + 2 < 5 - 7 
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # il s'agit d'une boucle. Pas de calcul du 3+2 car fait parti de la question
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"  # il s'agit d'une boucle. Pas de calcul du 3+2 car fait parti de la question
+
+puts "Ok, c'est faux alors !" # donnée simple
+
+puts "C'est drôle ça, faisons-en plus :" # donnée simple
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # c'est une donnée "booléen"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # c'est une donnée "booléen"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # c'est une donnée "booléen"
---
+++ ruby_exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+# la différence entre les 3 c'est qu'il manque des "" après puts user_name
+# pour l'exo 7.b les données ne sont pas dans le bon ordre
